b749dc703c50cc1cb6395571a98295fbcb9ef2ff
Jean Gravier <jean.gravier@icloud.com>
2018-02-08 16:59:18 +0100

    Updated gitignore

---
+++ libPuff/inc/OpenGL.hh
@@ -0,0 +1,13 @@
+//
+//  OpenGL.hh
+//  bomberman
+//
+//  Created by bobbyghetto on 08/02/2018.
+//  Copyright © 2018 bobbyghetto. All rights reserved.
+//
+
+#ifndef OpenGL_h
+#define OpenGL_h
+
+
+#endif /* OpenGL_h */
